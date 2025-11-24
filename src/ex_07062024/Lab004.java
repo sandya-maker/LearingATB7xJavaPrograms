6b9974e3e03b3265d63da15f64dce00fd75ea16b
sandya-maker <sandeshharne502@gmail.com>
2025-11-24 15:50:22 +0530

    All new files

--- src/ex_07062024/Lab004.java
+++ src/ex_07062024/Lab004.java
@@ -2,6 +2,6 @@ package ex_07062024;
 
 public class Lab004 {
     public static void main(String[] args) {
-        System.out.println("I am a sandesh");
+        System.out.println("I am sandesh");
     }
 }
